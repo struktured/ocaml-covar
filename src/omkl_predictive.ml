5e7354e7534cc441b8d1c0df1c334a1841c1245b
Carmelo Piccione <carmelo.piccione@gmail.com>
2016-07-09 14:10:10 -0400

    ring buffered predictive function

--- src/omkl_predictive.ml
+++ src/omkl_predictive.ml
@@ -9,22 +9,40 @@ module Instance = Kernel.Instance
 val predict : t -> Instance.t -> float
 end
 
+module type S_buffered =
+sig
+  include S
+  val empty : ?init_buffer_size:int -> ?bounded_buffer:bool -> Kernel.t -> t
+end
 
-module Make(Kernel:Omkl_kernel.S) :
-  S with module Kernel = Kernel =
+module Ring_buffered(Kernel:Omkl_kernel.S with module Instance = Omkl_instance.Float) :
+  S_buffered with module Kernel = Kernel =
 struct
   module Kernel = Kernel
   module Instance = Kernel.Instance
-  type t = {weights:Vec.t; instances:Instance.t array; kernel:Kernel.t} [@@deriving make]
+  module Instance_buffer = CCRingBuffer.Make(Instance)
+  module Weights_buffer = CCRingBuffer.Make(Float)
+  let default_buffer_size = 1000
+  let default_bounded_buffer = false
+  type t = {weights:Weights_buffer.t; instances:Instance_buffer.t; kernel:Kernel.t} [@@deriving make]
 
-  let predict t x = Array.map t.instances
-      ~f:(fun x' -> Kernel.apply t.kernel x' x) |>
+  let empty ?(init_buffer_size=default_buffer_size)
+    ?(bounded_buffer=default_bounded_buffer) kernel = make
+      ~weights:(Weights_buffer.create init_buffer_size ~bounded:bounded_buffer)
+      ~instances:(Instance_buffer.create init_buffer_size ~bounded:bounded_buffer)
+      ~kernel
+
+  let predict (t:t) (x:Instance.t) =
+    let instances : Float.t array = Instance_buffer.to_array t.instances in instances |>
+      Array.map ~f:(fun x' -> Kernel.apply t.kernel x' x) |>
       Vec.of_array |>
-      Lacaml_D.dot t.weights
+      Lacaml_D.dot (Weights_buffer.to_array t.weights |> Vec.of_array)
 
-  let add_support t ~weight x =
+  let add_support (t:t) ~weight instance =
     if weight = 0. then t else
-      {t with
-       weights = Vec.append t.weights (Lacaml_D.Vec.of_array [|weight|]);
-       instances = Array.append t.instances [|x|]}
+      begin
+        Weights_buffer.push_back t.weights weight;
+        Instance_buffer.push_back t.instances instance;
+        t
+      end
 end
