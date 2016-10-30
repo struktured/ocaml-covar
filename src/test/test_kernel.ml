2a86746916757f7374b7691b9efc979a8e4a5554
carm <carmelo.piccione@gmail.com>
2016-10-30 02:39:58 -0400

    improving test infra

--- src/test/test_kernel.ml
+++ src/test/test_kernel.ml
@@ -42,40 +42,66 @@ struct
 
 end
 
+module Test_params = struct
+module type S =
+sig
+  val equal_to : float -> float -> float
+  val title_tag : string
+end
 
-module Squared_exponential_test =
+end
+module Make(K: Kernel.S)(T:Test_params.S) = 
 struct
  module K = Squared_exponential
  module Comparison_test = Comparison_test(K)
  let kernel = K.create ()
  let one_when_same =
-   Comparison_test.for_k_x_x ~kernel
-      ?trials:None ~title_tag:"squared_exponential" ~equal_to:(fun _ -> 1.0)
+   Comparison_test.for_k_x_x' ~kernel
+      ?trials:None ~title_tag:T.title_tag ~equal_to:T.equal_to
  let tests = [one_when_same]
-end
-
-module Matern_test =
-struct
- module K = Matern
- module Comparison_test = Comparison_test(K)
- let kernel = K.create ()
- let one_when_same =
-   Comparison_test.for_k_x_x ~kernel
-      ?trials:None ~title_tag:"matern" ~equal_to:(fun _ -> 1.0)
- let tests = [one_when_same]
-end
 
 
-module Brownian_test =
-struct
- module K = Brownian
- module Comparison_test = Comparison_test(K)
- let kernel = K.create ()
- let one_when_same =
-   Comparison_test.for_k_x_x ~kernel
-      ?trials:None ~title_tag:"matern" ~equal_to:(fun _ -> 1.0)
- let tests = [one_when_same]
 end
+
+let equal_to_one _ _ = 1.0
+
+module Squared_exponential_test = Make
+  (Squared_exponential)
+  (struct
+    let equal_to = equal_to_one
+    let title_tag = "Squared exponential"
+   end)
+
+module Matern_test = Make
+  (Matern)
+  (struct
+    let equal_to = equal_to_one
+    let title_tag = "Matern"
+   end)
+
+
+module Brownian_test = Make
+  (Brownian)
+  (struct
+    let equal_to = Float.min
+    let title_tag = "Brownian"
+   end)
+
+module Periodic_test = Make
+  (Periodic)
+  (struct
+    let equal_to = equal_to_one
+    let title_tag = "Periodic"
+   end)
+
+
+module Linear_test = Make
+  (Linear)
+  (struct
+    let args = Some (Linear.Optional_args.make ~bias:0.0 ())
+    let equal_to = 1.0
+    let title_tag = "Periodic"
+   end)
 
 
 
